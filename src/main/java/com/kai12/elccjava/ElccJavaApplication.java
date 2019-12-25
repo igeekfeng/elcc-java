36327a73a44a7e865691b660b293c7bec7b06e68
xuxiaofeng <xuxiaofeng@k12cloud.cn>
2019-12-25 16:08:06 +0800

    message

---
+++ src/main/java/com/kai12/elccjava/ElccJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kai12.elccjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElccJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElccJavaApplication.class, args);
+    }
+
+}
